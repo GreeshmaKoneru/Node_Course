60dfc15eaac1c5ef11b2b688232fe4f6f130b0ee
GreeshmaKoneru <gkoneru@gmu.edu>
2025-10-25 20:31:32 -0400

    changed console log stment

--- NPM/npmtest.js
+++ NPM/npmtest.js
@@ -2,7 +2,7 @@ const figlet = require('figlet')
 
 figlet("Hello World!!", function (err, data) {
   if (err) {
-    console.log("Something went wrong...");
+    console.log("Something went wrong here");
     console.dir(err);
     return;
   }
